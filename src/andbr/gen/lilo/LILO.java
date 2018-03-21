19baf7a9c937fbe3edcaf6e10c239a0439316ded
andbr22 <abryja@gmail.com>
2018-03-21 17:33:27 +0100

    lilo added

--- src/andbr/gen/lilo/LILO.java
+++ src/andbr/gen/lilo/LILO.java
@@ -6,6 +6,40 @@ public class LILO<T> {
     private int size;
 
     public LILO() {
-        
+        this.size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void push(T newValue){
+        Node newNode = new Node();
+        newNode.setValue(newValue);
+        if(top == null){
+            top = newNode;
+            bottom = newNode;
+        }else{
+            top.setNext(newNode);
+            top = newNode;
+        }
+        size++;
+    }
+
+    public T pop(){
+        if(bottom == null){
+            return null;
+        }else{
+            T helper = bottom.getValue();
+            if(bottom == top){
+                bottom = null;
+                top = null;
+            }else{
+                bottom = bottom.getNext();
+            }
+            size--;
+            return helper;
+        }
+
     }
 }
--- src/andbr/gen/lilo/Node.java
+++ src/andbr/gen/lilo/Node.java
@@ -1,4 +1,25 @@
 package andbr.gen.lilo;
 
 public class Node<T> {
+    private Node next;
+    private T value;
+
+    public Node() {
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
 }
